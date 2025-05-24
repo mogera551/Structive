5c74d8463cad23d62d1b55d44a9bf8fe6cf0bc2a
mogera551 <103912708+mogera551@users.noreply.github.com>
2025-05-24 12:30:43 +0900

    refactor getListIndex

--- dist/StateClass/getListIndex.js
+++ dist/StateClass/getListIndex.js
@@ -1,27 +1,24 @@
 import { raiseError } from "../utils.js";
 import { GetContextListIndexSymbol } from "./symbols";
 export function getListIndex(info, receiver, handler) {
-    if (info.wildcardType === "none") {
-        return null;
+    switch (info.wildcardType) {
+        case "none":
+            return null;
+        case "context":
+            const lastWildcardPath = info.info.lastWildcardPath ??
+                raiseError(`lastWildcardPath is null`);
+            return receiver[GetContextListIndexSymbol](lastWildcardPath) ??
+                raiseError(`ListIndex not found: ${info.info.pattern}`);
+        case "all":
+            let parentListIndex = null;
+            for (let i = 0; i < info.info.wildcardCount; i++) {
+                const wildcardParentPattern = info.info.wildcardParentInfos[i] ?? raiseError(`wildcardParentPattern is null`);
+                const listIndexes = Array.from(handler.engine.getListIndexesSet(wildcardParentPattern, parentListIndex) ?? []);
+                const wildcardIndex = info.wildcardIndexes[i] ?? raiseError(`wildcardIndex is null`);
+                parentListIndex = listIndexes[wildcardIndex] ?? raiseError(`ListIndex not found: ${wildcardParentPattern.pattern}`);
+            }
+            return parentListIndex;
+        case "partial":
+            raiseError(`Partial wildcard type is not supported yet: ${info.info.pattern}`);
     }
-    else if (info.wildcardType === "context") {
-        const lastWildcardPath = info.info.lastWildcardPath ??
-            raiseError(`lastWildcardPath is null`);
-        return receiver[GetContextListIndexSymbol](lastWildcardPath) ??
-            raiseError(`ListIndex not found: ${info.info.pattern}`);
-    }
-    else if (info.wildcardType === "all") {
-        let parentListIndex = null;
-        for (let i = 0; i < info.info.wildcardCount; i++) {
-            const wildcardParentPattern = info.info.wildcardParentInfos[i] ?? raiseError(`wildcardParentPattern is null`);
-            const listIndexes = Array.from(handler.engine.getListIndexesSet(wildcardParentPattern, parentListIndex) ?? []);
-            const wildcardIndex = info.wildcardIndexes[i] ?? raiseError(`wildcardIndex is null`);
-            parentListIndex = listIndexes[wildcardIndex] ?? raiseError(`ListIndex not found: ${wildcardParentPattern.pattern}`);
-        }
-        return parentListIndex;
-    }
-    else if (info.wildcardType === "partial") {
-        raiseError(`Partial wildcard type is not supported yet: ${info.info.pattern}`);
-    }
-    return null;
 }
