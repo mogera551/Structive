e84b4372fd0020d54825bc19e0533954f822392a
mogera551 <103912708+mogera551@users.noreply.github.com>
2025-06-23 07:07:43 +0900

    feat: IStateInfoインターフェースに依存関係マップとリストインデックスを追加

--- src/StateInfo/types.ts
+++ src/StateInfo/types.ts
@@ -1,3 +1,6 @@
+import { IDependencyEdge } from "../DependencyWalker/types";
+import { IListIndex } from "../ListIndex/types";
+
 /**
  * StateClassに関する情報を表すインターフェース。
  */
@@ -15,4 +18,17 @@ export interface IStateInfo {
    * getter/setterを持つパスの集合。
    */
   accessors: Set<string>;
+
+  /**
+   * 依存関係のマップ。
+   * キーはパス名、値はそのパスに依存するエッジのセットです。
+   */
+  dependentTree: Map<string, Set<IDependencyEdge>>;
+
+  listIndexByList: WeakMap<any[], Set<IListIndex>>; // リストインデックスを持つリストのマップ
+
+  /**
+   * リストパス
+   */
+  listPaths: Set<string>;
 }
