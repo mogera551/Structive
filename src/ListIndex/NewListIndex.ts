cda315b9f7db752047381c7463d347b209e42bf2
mogera551 <mogera551@gmail.com>
2025-09-03 18:29:19 +0900

    refactoring2

--- src/ListIndex/NewListIndex.ts
+++ src/ListIndex/NewListIndex.ts
@@ -5,11 +5,14 @@ interface INewListIndex extends IListIndex {
 }
 
 let version = 0;
+let id = 0;
 class NewListIndex implements INewListIndex {
   #parentListIndex: INewListIndex | null = null;
   #pos: number = 0;
   #index: number = 0;
   #version: number;
+  #id = ++id;
+  #sid = this.#id.toString();
   constructor(parentListIndex: INewListIndex | null, index: number) {
     this.#parentListIndex = parentListIndex;
     this.#pos = parentListIndex ? parentListIndex.position + 1 : 0;
@@ -17,8 +20,24 @@ class NewListIndex implements INewListIndex {
     this.#version = version;
   }
 
+  get parentListIndex() {
+    return this.#parentListIndex;
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get sid() {
+    return this.#sid;
+  }
+
   get position() {
     return this.#pos;
+  }
+
+  get length() {
+    return this.#pos + 1;
   }
 
   get index() {
@@ -43,9 +62,32 @@ class NewListIndex implements INewListIndex {
     } else {
       if (typeof this.#indexes === "undefined" || this.#parentListIndex.version > this.#version) {
         this.#indexes = [...this.#parentListIndex.indexes, this.#index];
+        this.#version = version;
       }
     }
     return this.#indexes;
+  }
+
+  #listIndexes: WeakRef<IListIndex>[] | undefined;
+  get listIndexes(): WeakRef<IListIndex>[] {
+    if (this.#parentListIndex === null) {
+      if (typeof this.#listIndexes === "undefined") {
+        this.#listIndexes = [new WeakRef(this)];
+      }
+    } else {
+      if (typeof this.#listIndexes === "undefined") {
+        this.#listIndexes = [...this.#parentListIndex.listIndexes, new WeakRef(this)];
+      }
+    }
+    return this.#listIndexes;
+  }
+
+  at(pos: number): IListIndex | null {
+    if (pos >= 0) {
+      return this.listIndexes[pos].deref() || null;
+    } else {
+      return this.listIndexes[this.listIndexes.length + pos].deref() || null;
+    }
   }
 }
 
