9bf3e62e31144d00057f349f658ba767e51f7f06
mogera551 <mogera551@coo-d.jp>
2025-09-05 07:32:42 +0900

    feat: add Updater2 class and IUpdater2 interface for state management

---
+++ src/Updater2/Updater2.ts
@@ -0,0 +1,67 @@
+import { IComponentEngine } from "../ComponentEngine/types";
+import { IBinding } from "../DataBinding/types";
+import { IListIndex2 } from "../ListIndex2/types";
+import { ILoopContext } from "../LoopContext/types";
+import { createReadonlyStateProxy } from "../StateClass/createReadonlyStateProxy";
+import { IReadonlyStateProxy, IWritableStateProxy } from "../StateClass/types";
+import { useWritableStateProxy } from "../StateClass/useWritableStateProxy";
+import { IStructuredPathInfo } from "../StateProperty/types";
+import { IStatePropertyRef } from "../StatePropertyRef/types";
+import { IUpdateInfo, IUpdater2 } from "./types";
+
+/**
+ * Updater2クラスは、状態管理と更新の中心的な役割を果たします。
+ * 状態更新が必要な場合に、都度インスタンスを作成して使用します。
+ * 主な機能は以下の通りです:
+ */
+class Updater2 implements IUpdater2 {
+  queue: Array<IUpdateInfo> = [];
+  #updating: boolean = false;
+  #rendering: boolean = false;
+  #readonlyState: IReadonlyStateProxy | null = null;
+  
+  // Ref情報をキューに追加
+  enqueueRef(info: IStructuredPathInfo, listIndex: IListIndex2 | null, value: any): void {
+    this.queue.push({ info, listIndex, value });
+    if (this.#rendering) return;
+    this.#rendering = true;
+    queueMicrotask(() => {
+      this.rendering();
+    });
+  }
+  // 状態更新開始
+  async beginUpdate(engine: IComponentEngine, state: object, loopContext: ILoopContext | null, callback: (state: IWritableStateProxy) => Promise<void>): Promise<void> {
+    try {
+      this.#updating = true;
+      this.#readonlyState = createReadonlyStateProxy(engine, state);
+      await useWritableStateProxy(engine, state, loopContext, async (state:IWritableStateProxy) => {
+        // 状態更新処理
+        await callback(state);
+      });
+    } finally {
+      this.#updating = false;
+    }
+  }
+  // レンダリング
+  rendering(): void {
+    try {
+      while( this.queue.length > 0 ) {
+        // レンダリング処理
+        const queue = this.queue;
+        this.queue = [];
+        // 各キューに対してレンダリング処理を実行
+        this.renderItems(queue);
+      }
+    } finally {
+      this.#rendering = false;
+    }
+  }
+
+  renderItems(items: Array<IUpdateInfo>): void {
+    // 実際のレンダリングロジックをここに実装
+    if (!this.#readonlyState) return;
+    // 
+    const updatedBindings: Set<IBinding> = new Set();
+  }
+
+}
---
+++ src/Updater2/types.ts
@@ -0,0 +1,22 @@
+import { IComponentEngine } from "../ComponentEngine/types";
+import { IListIndex2 } from "../ListIndex2/types";
+import { ILoopContext } from "../LoopContext/types";
+import { IReadonlyStateProxy, IStructiveState, IWritableStateProxy } from "../StateClass/types";
+import { IStructuredPathInfo } from "../StateProperty/types";
+import { IStatePropertyRef } from "../StatePropertyRef/types";
+
+export type IUpdateInfo = {
+  info: IStructuredPathInfo;
+  listIndex: IListIndex2 | null;
+  value: any;
+}
+
+export interface IUpdater2 {
+  queue: Array<IUpdateInfo>;
+  // Ref情報をキュー
+  enqueueRef(info: IStructuredPathInfo, listIndex: IListIndex2 | null, value: any): void;
+  // 状態更新開始
+  beginUpdate(engine: IComponentEngine, state: object, loopContext: ILoopContext | null, callback: (state: IWritableStateProxy) => Promise<void> | void): Promise<void>;
+  // レンダリング
+  rendering(): void;
+}
